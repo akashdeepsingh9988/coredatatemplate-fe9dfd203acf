ba64c700d82adb0986ae3251edeac514b89603fd
MacStudent <macstudent@128-imac14.local>
2018-11-12 10:47:32 -0500

    new hghghghg

--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -114,7 +114,7 @@ class ViewController: UIViewController {
             print("Error when fetching from database")
         }
     }
-    
+    //hgggh
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
